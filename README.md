5c9440cca6076fe60b23fbe9a2c0f6b6e74db7d5
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2019-12-13 15:39:14 +0100

    Added proxy generator script.

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Based on [proxy-login-automator](https://github.com/sjitech/proxy-login-automato
 Supported configuration variables are:
 
 * `REMOTE_HOST`, a comma seperated list of hostnames, including the ports, e.g. `proxy1.proxy.com:1234,proxy2.proxy.com:1235`. The proxies are exposed via ports `8080` up to `9080` in the order they were given. 
+* `GET_REMOTE_HOST`, alternatively, a script to generate a list of proxies, e.g. `for i in \$(seq 1000 1 1100); do printf \"proxy%i.proxy.com:%i,\" $i $i; done`
 * `REMOTE_USER`
 * `REMOTE_PASSWORD`
 * `REMOTE_HTTPS`
--- launcher.sh
+++ launcher.sh
@@ -1,22 +1,33 @@
 #! /bin/bash
 set -euo pipefail
 
+env
+
 # Check environment
-if [ -z "${REMOTE_HOST:-}" ]; then
+if [ -n "${GET_REMOTE_HOST}" ]; then
+  echo "GET_REMOTE_HOST script was supplied evaluating, and overriding REMOTE_HOST variable" 
+  REMOTE_HOST=$(eval ${GET_REMOTE_HOST})
+fi
+
+if [ -z "${REMOTE_HOST}" ]; then
   echo "REMOTE_HOST was not set"
 fi
 
-if [ -z "${REMOTE_USER:-}" ]; then
+if [ -z "${REMOTE_USER}" ]; then
   echo "REMOTE_USER was not set"
 fi
 
-if [ -z "${REMOTE_PASSWORD:-}" ]; then
+if [ -z "${REMOTE_PASSWORD}" ]; then
   echo "REMOTE_PASSWORD was not set"
 fi
 
 LOCAL_PORT=8080
 for HOST_AND_PORT in $(echo $REMOTE_HOST| sed "s/,/ /g")
 do
+  if [ -z "${HOST_AND_PORT}" ]; then
+    continue
+  fi
+
   HOST=$(echo $HOST_AND_PORT | cut -f1 -d:)
   PORT=$(echo $HOST_AND_PORT | cut -f2 -d:)
   
